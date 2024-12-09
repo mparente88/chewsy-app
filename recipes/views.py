26a7a5a406f5bf6fdb6787789077aa42ceee32da
mparente88 <parente.molly@gmail.com>
2024-12-09 13:33:47 -0800

    detail view made

--- recipes/views.py
+++ recipes/views.py
@@ -20,3 +20,29 @@ class RecipeListCreateView(View):
         )
         return JsonResponse({"id": recipe.id, "message": "Recipe created successfully!"})
 
+class RecipeDetailView(View):
+    def get(self, request, pk):
+        recipe = get_object_or_404(Recipe, pk=pk)
+        recipe_data = {
+            "id": recipe.id,
+            "title": recipe.title,
+            "description": recipe.description,
+            "prep_time": recipe.prep_time,
+            "cook_time": recipe.cook_time,
+        }
+        return JsonResponse(recipe_data)
+    
+    def put(self, request, pk):
+        recipe = get_object_or_404(Recipe, pk=pk)
+        data = request.POST
+        recipe.title = data.get("title", recipe.title)
+        recipe.description = data.get("description", recipe.description)
+        recipe.prep_time = data.get("prep_time", recipe.prep_time)
+        recipe.cook_time = data.get("cook_time", recipe.cook_time)
+        recipe.save()
+        return JsonResponse({"message": "Recipe updated successfull!"})
+    
+    def delete(self, request, pk):
+        recipe = get_object_or_404(Recipe, pk=pk)
+        recipe.delete()
+        return JsonResponse({"message": "Recipe deleted successfull!"})
