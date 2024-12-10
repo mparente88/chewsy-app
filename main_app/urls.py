bd39986fd2b7a70055f3c6af9bebca887b013c74
mparente88 <parente.molly@gmail.com>
2024-12-10 10:21:24 -0800

    basic templates made, views added

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
-from . import views
+from .views import (
+    RecipeListView, RecipeDetailView, RecipeCreateView, RecipeUpdateView, RecipeDeleteView,
+    IngredientCreateView, IngredientUpdateView, IngredientDeleteView,
+    TagListView
+)
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about')
+    path('', RecipeListView.as_view(), name='recipe_list'),
+    path('recipe/<int:pk>/', RecipeDetailView.as_view(), name='recipe_detail'),
+    path('recipe/new/', RecipeCreateView.as_view(), name='recipe_create'),
+    path('recipe/<int:pk>/edit/', RecipeUpdateView.as_view(), name='recipe_update'),
+    path('recipe/<int:pk>/delete/', RecipeDeleteView.as_view(), name='recipe_delete'),
+    path('recipe/<int:recipe_id>/ingredient/new/', IngredientCreateView.as_view(), name='ingredient_create'),
+    path('ingredient/<int:pk>/edit/', IngredientUpdateView.as_view(), name='ingredient_update'),
+    path('ingredient/<int:pk>/delete/', IngredientDeleteView.as_view(), name='ingredient_delete'),
+    path('tags/', TagListView.as_view(), name='tag_list'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,96 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+from .models import Recipe, Ingredient, Tag
+from .forms import RecipeForm, IngredientForm
 
-def home(request):
-    return HttpResponse('<h1>Hello</h1>')
+# Recipes
 
-def about(request):
-    return render(request, 'about.html')
+class RecipeListView(LoginRequiredMixin, ListView):
+    model = Recipe
+    template_name = 'recipes/recipe_list.html'
+    context_object_name = 'recipes'
+
+    def get_queryset(self):
+        return Recipe.objects.filter(user=self.request.user)
+    
+class RecipeDetailView(LoginRequiredMixin, DetailView):
+    model = Recipe
+    template_name = 'recipes/recipe_detail.html'
+    context_object_name = 'recipe'
+
+    def get_queryset(self):
+        return Recipe.objects.filter(user=self.request.user)
+    
+class RecipeCreateView(LoginRequiredMixin, CreateView):
+    model = Recipe
+    form_class = RecipeForm
+    template_name = 'recipes/recipe_form.html'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse_lazy('recipe_detail', kwargs={'pk': self.object.pk})
+    
+class RecipeUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Recipe
+    form_class = RecipeForm
+    template_name = 'recipes/recipe_form.html'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        recipe = self.get_object()
+        return recipe.user == self.request.user
+    
+    def get_success_url(self):
+        return reverse_lazy('recipe_detail', kwargs={'pk': self.object.pk})
+    
+class RecipeDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Recipe
+    template_name = 'recipes/recipe_confirm_delete.html'
+    success_url = reverse_lazy('recipe_list')
+
+    def test_func(self):
+        recipe = self.get_object()
+        return recipe.user == self.request.user
+    
+# Ingredients
+
+class IngredientCreateView(LoginRequiredMixin, CreateView):
+    model = Ingredient
+    form_class = IngredientForm
+    template_name = 'recipes/ingredient_form.html'
+
+    def form_valid(self, form):
+        form.instance.recipe_id = self.kwargs['recipe_id']
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse_lazy('recipe_detail', kwargs={'pk': self.object.recipe_id})
+    
+class IngredientUpdateView(LoginRequiredMixin, UpdateView):
+    model = Ingredient
+    form_class = IngredientForm
+    template_name = 'recipes/ingredient_form.html'
+
+    def get_success_url(self):
+        return reverse_lazy('recipe_detail', kwargs={'pk': self.object.recipe_id})
+    
+class IngredientDeleteView(LoginRequiredMixin, DeleteView):
+    model = Ingredient
+    template_name = 'recipes/ingredient_confirm_delete.html'
+
+    def get_success_url(self):
+        return reverse_lazy('recipe_detail', kwargs={'pk': self.object.recipe_id})
+    
+# Tags
+
+class TagListView(LoginRequiredMixin, ListView):
+    model = Tag
+    template_name = 'recipes/tag_list.html'
+    context_object_name = 'tags'
