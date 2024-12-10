904d40fd3060f47974d36e13f1fa7c90a72cd2a4
mparente88 <parente.molly@gmail.com>
2024-12-10 10:03:20 -0800

    models registered in admin.py

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Recipe, Ingredient, Tag
 
-# Register your models here.
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'prep_time', 'cook_time', 'total_time', 'servings', 'created_at')
+    list_filter = ('user', 'created_at', 'tags')
+    search_fields = ('title', 'description')
+    autocomplete_fields = ('tags',)
+    prepopulated_fields = {'total_time': ('prep_time', 'cook_time')}
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'quantity', 'measurement', 'recipe')
+    list_filter = ('measurement',)
+    search_fields = ('name',)
+    ordering = ('recipe', 'order')
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category')
+    list_filter = ('category',)
+    search_fields = ('name',)
