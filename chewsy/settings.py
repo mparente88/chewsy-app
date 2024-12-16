814b260d879257e9b18d9fb16006335db7ac0df2
mparente88 <parente.molly@gmail.com>
2024-12-16 13:34:56 -0800

    added logging to settings.py

--- chewsy/settings.py
+++ chewsy/settings.py
@@ -150,3 +150,18 @@ LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 
 LOGOUT_REDIRECT_URL = '/login/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
