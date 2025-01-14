c87d49601d620fec22a89e21ad33f48800b4338f
mparente88 <parente.molly@gmail.com>
2025-01-14 13:39:34 -0800

    corrected logout url

--- main_app/urls.py
+++ main_app/urls.py
@@ -29,7 +29,7 @@ register_converter(SignedIntConverter, 'signed_int')
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), {'next_page': 'home'}, name='logout'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('signup/', SignUpView.as_view(), name='signup'),
     path('', HomeView.as_view(), name='home'),
     path('my-recipes/', MyRecipesListView.as_view(), name='my_recipes'),
